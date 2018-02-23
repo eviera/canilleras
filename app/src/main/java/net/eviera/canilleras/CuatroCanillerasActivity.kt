cc9a42d620a54fadee6c6bbd5311d858da460e6c
emi@asterix <emiliano.viera@gmail.com>
2018-02-23 09:46:09 -0300

    Prueba de plantillas con bitmap

--- app/src/main/java/net/eviera/canilleras/CuatroCanillerasActivity.kt
+++ app/src/main/java/net/eviera/canilleras/CuatroCanillerasActivity.kt
@@ -7,6 +7,11 @@ import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.widget.ImageView
 import kotlinx.android.synthetic.main.activity_cuatro_canilleras.*
+import android.opengl.ETC1.getHeight
+import android.opengl.ETC1.getWidth
+import android.graphics.Bitmap
+import android.graphics.Canvas
+
 
 class CuatroCanillerasActivity : AppCompatActivity() {
 
@@ -18,9 +23,23 @@ class CuatroCanillerasActivity : AppCompatActivity() {
         val bgBordeImageview = ContextCompat.getDrawable(this, R.drawable.bg_borde_imageview)
         val pele = ContextCompat.getDrawable(this, R.drawable.pele)
 
-        val layerDrawable = LayerDrawable(arrayOf<Drawable?>(bgBordeImageview, pele, plantillaFondoBlancoConEdit))
-        plantilla_arr_izq.setImageDrawable(layerDrawable)
-        //plantilla_arr_izq.setBackgroundResource(R.drawable.bg_borde_imageview)
+        //Con LayerDrawable
+        //val layerDrawable = LayerDrawable(arrayOf<Drawable?>(bgBordeImageview, pele, plantillaFondoBlancoConEdit))
+        //plantilla_arr_izq.setImageDrawable(layerDrawable)
+
+        //Con Bitmap
+        val layerDrawable = LayerDrawable(arrayOf<Drawable?>(pele, plantillaFondoBlancoConEdit))
+        val b = Bitmap.createBitmap(108, 192, Bitmap.Config.ARGB_8888)
+        layerDrawable.setBounds(0, 0, 108, 192)
+        layerDrawable.draw(Canvas(b))
+        /*
+            Paint p = new Paint();
+            p.setAntiAlias(true);
+            p.setFilterBitmap(true);
+            canvas.drawBitmap(someBitmap, new Matrix(), p);
+         */
+
+        plantilla_arr_izq.setImageBitmap(b)
 
         //frut
         val layerDrawableVacio = LayerDrawable(arrayOf<Drawable?>(bgBordeImageview, plantillaFondoBlancoConEdit))
