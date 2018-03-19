fff658179d3d3a33be32a62ef898257c7f34b110
emi@asterix <emiliano.viera@gmail.com>
2018-03-19 00:38:34 -0300

    Pruebas motionview

---
+++ app/src/main/java/net/eviera/canilleras/BORRARMotionView.kt
@@ -0,0 +1,12 @@
+package net.eviera.canilleras
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class BORRARMotionView : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_borrarmotion_view)
+    }
+}
