2c6701af047fb5720e954ac51ee39097c9667a1d
emi@asterix <emiliano.viera@gmail.com>
2018-05-23 23:42:40 -0300

    clases de entidad

---
+++ app/src/main/java/net/eviera/canilleras/entity/Project.kt
@@ -0,0 +1,9 @@
+package net.eviera.canilleras.entity
+
+class Project {
+
+    private var id: Long? = null
+    private var name: String? = null
+    private val templates = List(4) { Template() }
+
+}
---
+++ app/src/main/java/net/eviera/canilleras/entity/Template.kt
@@ -0,0 +1,4 @@
+package net.eviera.canilleras.entity
+
+class Template {
+}
